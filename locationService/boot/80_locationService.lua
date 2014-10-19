94b7cc407fa345cb7472a1779c6a6b6bad5af7dd
Eva Lacy <eva@lacy.ie>
2014-10-19 12:43:53 +0100

    Added locationService

---
+++ locationService/boot/80_locationService.lua
@@ -0,0 +1,34 @@
+local component = require("component")
+local sides = require("sides")
+local location = require("locationService")
+
+robot_move = component.robot.move
+robot_turn = component.robot.turn
+
+function component.robot.move(side)
+	robot_move(side)
+	local currentLocation = locationService.getLocation()
+	if sides.up == side then
+		currentLocation.pos.y = currentLocation.pos.y + 1
+	elseif sides.down == side then
+		currentLocation.pos.y = currentLocation.pos.y - 1
+	elseif sides.forward == side then
+		currentLocation.pos.x = currentLocation.pos.x + currentLocation.xDir
+		currentLocation.pos.z = currentLocation.pos.z + currentLocation.zDir
+	elseif sides.back == side then
+		currentLocation.pos.x = currentLocation.pos.x - currentLocation.xDir
+		currentLocation.pos.z = currentLocation.pos.z - currentLocation.zDir
+	end
+	locationService.setLocation(currentLocation)
+end
+
+function component.robot.turn(isRight)
+	robot_turn(isRight)
+	local location = locationService.getLocation()
+	if isRight then
+		location.xDir, location.zDir = -location.zDir, location.xDir
+	else
+		location.xDir, location.zDir = location.zDir, -location.xDir
+	end
+	locationService.setLocation(currentLocation)
+end
---
+++ locationService/lib/locationService.lua
@@ -0,0 +1,76 @@
+local locationService = {}
+local libvec = require("libvec")
+local currentLocation
+local CURRENTLOCATIONFILENAME = "/var/location/currentLocation"
+local DEFAULTLOCATION = {0,0,0,1,0}
+
+function serializeToFile(object, filename)
+	ensureStateDirExists()
+	local handle = fs.open(STATEDIR .. filename,"wb")
+	local objectString = serialization.serialize(object)
+	handle:write(objectString)
+	handle:close()
+	return true
+end
+function readFile(file)
+    local f = io.open(file, "rb")
+	if not f then 
+		return
+	end
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+function deserializeFromFile(filename)
+	ensureStateDirExists()
+	local objectString = readFile(STATEDIR .. filename)
+	if not objectString then
+		return
+	end
+	return serialization.unserialize(objectString)
+end
+function locationService.getLocationFromArray(array)
+	local location = { }
+	location.pos = libvec.new(tonumber(array[1]),tonumber(array[2]),tonumber(array[3]))
+	location.xDir = tonumber(array[4])
+	location.zDir = tonumber(array[5])
+	if location.pos.x and location.pos.y and location.pos.z and location.xDir and location.zDir then
+		if location.xDir > 1 or location.xDir < -1 or location.zDir > 1 or location.zDir < -1 then
+			error("Direction should be between 1 and -1")
+			return nil
+		else
+			return location
+		end
+	else
+		error("Invalid location entered")
+		return nil
+	end
+end
+
+function locationService.getLocation()
+	if not currentLocation then
+		currentLocation = deserializeFromFile(CURRENTLOCATIONFILENAME)
+		if not currentLocation then
+			return locationService.getLocationFromArray(DEFAULTLOCATION)
+		end
+	end
+	return currentLocation
+end
+function locationService.setLocation(location)
+	currentLocation = location
+	return serializeToFile(location,CURRENTLOCATIONFILENAME)
+end
+
+function locationService.setLocationFromArray(array)
+	local location = locationService.getLocationFromArray(array)
+	if location then
+		setLocation(location)
+		return true
+	else
+		return false
+	end
+end
+
+
+return locationService
