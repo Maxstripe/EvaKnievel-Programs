10bcedaba7096b7cf08d7ac7d73c9307ad7b70b8
Eva Lacy <eva@lacy.ie>
2014-10-19 18:14:46 +0100

    Removed remaining references to libvec

--- locationService/boot/80_locationService.lua
+++ locationService/boot/80_locationService.lua
@@ -10,15 +10,15 @@ function component.robot.move(side)
 	if result then
 		local currentLocation = locationService.getLocation()
 		if sides.up == side then
-			currentLocation.pos.y = currentLocation.pos.y + 1
+			currentLocation.y = currentLocation.y + 1
 		elseif sides.down == side then
-			currentLocation.pos.y = currentLocation.pos.y - 1
+			currentLocation.y = currentLocation.y - 1
 		elseif sides.forward == side then
-			currentLocation.pos.x = currentLocation.pos.x + currentLocation.xDir
-			currentLocation.pos.z = currentLocation.pos.z + currentLocation.zDir
+			currentLocation.x = currentLocation.x + currentLocation.xDir
+			currentLocation.z = currentLocation.z + currentLocation.zDir
 		elseif sides.back == side then
-			currentLocation.pos.x = currentLocation.pos.x - currentLocation.xDir
-			currentLocation.pos.z = currentLocation.pos.z - currentLocation.zDir
+			currentLocation.x = currentLocation.x - currentLocation.xDir
+			currentLocation.z = currentLocation.z - currentLocation.zDir
 		end
 		locationService.setLocation(currentLocation)
 	end
